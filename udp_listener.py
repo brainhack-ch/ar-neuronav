1f834baf09abbe43962ffcf15cc9b0e62f5cf345
Riccardo Poggi <poggi.ricky@gmail.com>
2019-11-09 05:43:04 +0100

    changed names

--- udp_listener.py
+++ udp_listener.py
@@ -2,7 +2,7 @@ import socket
 
 if __name__ == '__main__':
     import argparse
-    parser = argparse.ArgumentParser(description='UDP Sender')
+    parser = argparse.ArgumentParser(description='UDP Listener')
     parser.add_argument('--ip-address', default='127.0.0.1',
                         help='IP address')
     parser.add_argument('--port', '-p', type=int, default=5005,
